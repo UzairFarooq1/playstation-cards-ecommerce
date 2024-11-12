c46f825d8d42af58fd414e737adaf4624fff8a95
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2024-11-12 13:55:56 +0300

    fix: :bug:

--- next-auth.d.tsx
+++ next-auth.d.tsx
@@ -4,13 +4,6 @@ declare module "next-auth" {
   interface User {
     role?: string;
   }
-
-  interface Session {
-    user: User & {
-      id: string;
-      role?: string;
-    };
-  }
 }
 
 declare module "next-auth/jwt" {
