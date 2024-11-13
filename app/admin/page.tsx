bfcac14701a373736d522668331eacdf4afabc4e
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2024-11-13 21:22:49 +0300

    feat: :bug:

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,23 +1,6 @@
-// app/admin/page.tsx
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "../lib/auth";
 import AdminDashboard from "../components/AdminDashboard";
-import { redirect } from "next/navigation";
-import { checkUserRole } from "../lib/auth-utils";
 
-export default async function AdminPage() {
-  const session = await getServerSession(authOptions);
-
-  if (!session || !session.user) {
-    // Redirect unauthenticated users to the login page
-    return redirect("/login");
-  }
-
-  if (!(await checkUserRole(session.user, ["ADMIN"]))) {
-    // Redirect non-admin users to an unauthorized page
-    return redirect("/unauthorized");
-  }
-
-  // Render the AdminDashboard component
+// app/admin/page.tsx
+export default function AdminPage() {
   return <AdminDashboard />;
 }
